af7a509d46ddf24042b8e47c94966aa8aeca4d71
stoyanov.e <quick.es@gmail.com>
2024-07-06 12:09:03 +0300

    Добавил настройку pytds

--- v7client/mssql.py
+++ v7client/mssql.py
@@ -10,16 +10,17 @@ logging.getLogger("pytds").setLevel(logging.WARNING)
 
 class MsSqlDb:
 
-    def __init__(self, database, host, user='sa', password='', port=1433):
+    def __init__(self, database, host, user='sa', password='', port=1433, **connect_args):
         self.cnx = None
         self.host = host
         self.user = user
         self.password = password
         self.port = port
         self.database = database
+        self.connect_args = connect_args
 
     def connect(self):
-        return pytds.connect(self.host, self.database, self.user, self.password, port=self.port, bytes_to_unicode=True)
+        return pytds.connect(self.host, self.database, self.user, self.password, port=self.port, bytes_to_unicode=True, **self.connect_args)
 
     @contextmanager
     def query(self, sql):
